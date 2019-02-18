69fff7c5ed91b06bf771484e59fc756354d5b7b5
Dheeraj <dheer047@gmail.com>
2019-02-18 09:21:33 +0530

    Deleting commented code

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,9 +3,6 @@ const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 const path = require('path');
-//const beautify = require("gulp-beautify");
-//const filter = require('gulp-filter');
-//const jsFilter = filter(['**/*.js', '**/*.json'], { restore: true });
 
 module.exports = class extends Generator {
   prompting() {
@@ -30,95 +27,32 @@ module.exports = class extends Generator {
         message: 'Would you like to install Boilerplate with Swagger API?',
         default: true
       }
-      /*{
-        type: 'checkbox',
-        name: 'features',
-        message: 'What more would you like to generate?',
-        choices: [{
-          name: 'Boilerplate',
-          value: 'includeBoilerplate',
-          checked: true
-        },
-        {
-          name: 'Swagger API',
-          value: 'includeSwagger',
-          checked: false
-        }
-        ]
-      }*/
     ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
-      /*if(this.props.features.includes('includeBoilerplate')){
-        this.includeBoilerplate = true;
-      } else {
-        this.includeBoilerplate = false;
-      }
-      if(this.props.features.includes('includeSwagger')){
-        this.includeSwagger = true;
-      } else {
-        this.includeSwagger = false;
-      }
-      this.registerTransformStream(jsFilter);
-      this.registerTransformStream(beautify({ indent_size: 2 }));
-      this.registerTransformStream(jsFilter.restore);
-      this.log(this.props.features)*/
     });
   }
 
   writing() {
     
-    //if(this.props.features.includes('includeBoilerplate')){
     if(this.props.install){
       this.fs.copy(
         this.templatePath('_README.md'),
         this.destinationPath('README.md')
       );
-
-      /*if(this.props.features.includes('includeSwagger')){
-
-        this.fs.copyTpl(
-          this.templatePath('_package.json'),
-          this.destinationPath('package.json'),
-          {
-            title: this.props.title,
-            swaggerTest: data.swaggerTest,
-            swaggerDependencies: data.swaggerDependencies
-          }
-        );
-
-        this.fs.copyTpl(
-          this.templatePath('server/manifest.js'),
-          this.destinationPath('server/manifest.js'),
-          {
-            swaggerRequire: this.includeSwagger,
-            swaggerRegister: data.swaggerRegister
-          }
-        );
-
-      } else {*/
-        this.fs.copyTpl(
-          this.templatePath('_package.json'),
-          this.destinationPath('package.json'),
-          {
-            title: this.props.title,
-            //swaggerTest: "",
-            //swaggerDependencies: ""
-          }
-        );
-
-        this.fs.copyTpl(
-          this.templatePath('server/manifest.js'),
-          this.destinationPath('server/manifest.js'),
-          /*{
-            swaggerRequire: "",
-            swaggerRegister: ""
-          }*/
-        );
-      //}
-
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'),
+        {
+          title: this.props.title,
+        }
+      );
+      this.fs.copyTpl(
+        this.templatePath('server/manifest.js'),
+        this.destinationPath('server/manifest.js'),
+      );
       this.fs.copy(
         this.templatePath('_npmignore'),
         this.destinationPath('.npmignore')
