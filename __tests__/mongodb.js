379b28d87577058da634c738bf6731401e180fd3
Dheeraj <dheer047@gmail.com>
2019-02-20 12:13:08 +0530

    added tests

---
+++ __tests__/mongodb.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+
+describe('Test | Boilerplate with Swagger API and MongoDB', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      name: 'generator-temp',
+      install: true,
+      database: ['includeMongodb']
+    });
+  });
+
+  it('Creates Boilerplate template with Swagger API and MongoDB', () => {
+    const expected = [
+      '.eslintrc.json',
+      '.gitignore',
+      '.npmignore',
+      'README.md',
+      'package.json',
+      'test/index.js',
+      'server/index.js',
+      'server/manifest.js',
+      'server/.env-keep',
+      'lib/routes/.gitkeep',
+      'lib/.hc.js',
+      'lib/index.js',
+      'lib/models/.gitkeep'
+    ];
+    assert.file(expected);
+  });
+});
---
+++ __tests__/postgresql.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+
+describe('Test | Boilerplate with Swagger API and PostgreSQL', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      name: 'generator-temp',
+      install: true,
+      database: ['includePsql']
+    });
+  });
+
+  it('Creates Boilerplate template with Swagger API and PostgreSQL', () => {
+    const expected = [
+      '.eslintrc.json',
+      '.gitignore',
+      '.npmignore',
+      'README.md',
+      'package.json',
+      'test/index.js',
+      'server/index.js',
+      'server/manifest.js',
+      'server/.env-keep',
+      'lib/routes/.gitkeep',
+      'lib/.hc.js',
+      'lib/index.js',
+      'lib/migrations/.gitkeep',
+      'lib/models/.gitkeep',
+      'lib/plugins/schwifty.js',
+      'knexfile.js'
+    ];
+    assert.file(expected);
+  });
+});
