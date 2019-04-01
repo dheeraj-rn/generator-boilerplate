e93377e2b102b2d46078c9e55cf9ea7539804c0e
Dheeraj <dheer047@gmail.com>
2019-04-01 12:56:34 +0530

    initial commit

---
+++ server/manifest.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('confidence');
+const Toys = require('toys');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Package = require('../package.json');
+
+// Pull .env into process.env
+Dotenv.config({ path: `${__dirname}/.env` });
+
+const mongoURL = 'mongodb://' + process.env.MONGO_DB_USER + ':' + process.env.MONGO_DB_PASS + '@' + process.env.MONGO_DB_HOST + '/' + process.env.MONGO_DB_NAME;
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: 'localhost',
+        port: {
+            $env: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: { $env: 'NODE_ENV' },
+            $default: {
+                log: ['error'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {  mongoURI: mongoURL  }
+            },
+            {
+                plugin: {
+                    $filter: { $env: 'NODE_ENV' },
+                    $default: 'hpal-debug',
+                    production: Toys.noop
+                }
+            },
+            {
+                plugin: Inert,
+                options: {}
+            },
+            {
+                plugin: Vision,
+                options: {}
+            },
+            {
+                plugin: HapiSwagger,
+                options: {
+                    info: {
+                        title: 'API Documentation',
+                        version: Package.version
+                    }
+                }
+            },
+            {
+                plugin: 'schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'pg',
+                            connection: {
+                                host : process.env.PG_DB_HOST,
+                                user : process.env.PG_DB_USER,
+                                password : process.env.PG_DB_PASS,
+                                database : process.env.PG_DB_NAME
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
+            }
+        ]
+    }
+});
